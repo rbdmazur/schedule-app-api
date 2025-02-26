275cf7f35426b24e2584d8b01a2c35dd4e3a0f1f
Daniil Zhuk <rbdmazur@gmail.com>
2025-02-26 14:55:23 +0300

    Serialization refactor. Routes and security deleted

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,33 +1,14 @@
 package com.example
 
-import com.example.auth.configureAuth
 import com.example.repository.configureDatabase
-import com.example.repository.repositories.ScheduleRepository
-import com.example.repository.repositories.UsersRepository
-import com.example.routes.*
-import com.example.testdata.configureTest
-import io.ktor.serialization.kotlinx.json.*
+import com.example.serialization.configureSerialization
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.routing.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
-    val usersRepository = UsersRepository()
-    val scheduleRepository = ScheduleRepository()
-    install(ContentNegotiation) {
-        json()
-    }
+    configureSerialization()
     configureDatabase()
-    configureAuth(usersRepository)
-
-    routing {
-        authRoutes()
-        schedulesRoutes(scheduleRepository)
-        subjectRoutes(scheduleRepository, usersRepository)
-        userRoutes(usersRepository)
-    }
 }
